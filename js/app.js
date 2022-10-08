d4e6329fea54109a90119d55853a18f04148a52c
ayihan <ayihanhaq@gmail.com>
2022-10-08 17:05:05 +0500

    Added form submition conformation

--- js/app.js
+++ js/app.js
@@ -1,37 +1,25 @@
-// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-// const supabase = createClient('https://zjesbzduygemrdprcvvq.supabase.co',
-// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZXNiemR1eWdlbXJkcHJjdnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTUyNDMsImV4cCI6MTk4MDU3MTI0M30.wtkQQpQMKYsjYyxG8PA4bcGY5OHEcVaywHuAYoIQLh0')
-
-// console.log('Supabase Instance: ', supabase)
-
-// const { createClient } = supabase
-// const _supabase = createClient('https://zjesbzduygemrdprcvvq.supabase.co',
-// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZXNiemR1eWdlbXJkcHJjdnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTUyNDMsImV4cCI6MTk4MDU3MTI0M30.wtkQQpQMKYsjYyxG8PA4bcGY5OHEcVaywHuAYoIQLh0'
-// )
-
-// console.log('Supabase Instance: ', _supabase)
-// const supabase = createClient('https://zjesbzduygemrdprcvvq.supabase.co',
-// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZXNiemR1eWdlbXJkcHJjdnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTUyNDMsImV4cCI6MTk4MDU3MTI0M30.wtkQQpQMKYsjYyxG8PA4bcGY5OHEcVaywHuAYoIQLh0'
-// )
-
-// 'https://zjesbzduygemrdprcvvq.supabase.co',
-// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZXNiemR1eWdlbXJkcHJjdnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTUyNDMsImV4cCI6MTk4MDU3MTI0M30.wtkQQpQMKYsjYyxG8PA4bcGY5OHEcVaywHuAYoIQLh0'
-
 const client = supabase.createClient(
 	'https://zjesbzduygemrdprcvvq.supabase.co',
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZXNiemR1eWdlbXJkcHJjdnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTUyNDMsImV4cCI6MTk4MDU3MTI0M30.wtkQQpQMKYsjYyxG8PA4bcGY5OHEcVaywHuAYoIQLh0'
 );
 
 const form = document.querySelector('form')
-let checker = ""
+let checker = []
 
 function checkBox(service) {
-	checker = service
+	if (!checker.includes(service)) {
+		checker.push(service)
+	}
 }
 
 async function sendReq() {
 	form.addEventListener('submit', (event) => {
 		event.preventDefault()
+		const button = document.getElementById('send-btn')
+		button.innerHTML = 'Request Sent'
+		setTimeout(() => {
+			button.innerHTML = 'Send'
+		}, 5000)
 		const formData = new FormData(form)
 		const request = {
 			Name: formData.get('name'),
@@ -42,11 +30,13 @@ async function sendReq() {
 		console.log(request)
 		client
 			.from('Requests')
-			.insert(request)
+			.insert(request, { upsert: true })
 			.then((response) => {
 				console.log(response)
 			})
+		checker = []
 	})
+
 
 	// for debugging purposes
 	// const { data, error } = await client
@@ -56,5 +46,4 @@ async function sendReq() {
 	// 	])
 	// 	console.log(data, error)
 }
-
 sendReq()
