88100454fc184dd853873936de5884df6dd8ea5e
ayihan <ayihanhaq@gmail.com>
2022-10-17 21:50:11 +0500

    added signup integration for Volunteers

--- js/signup.js
+++ js/signup.js
@@ -38,45 +38,71 @@ async function specialization(obj) {
 				const id = response.data[0].id
 				client
 					.from('Volunteers')
-					.insert([{ id: id }])
+					.insert([{ id: id, Department: obj.Role }])
 					.then((response) => {
-						// volunteersSpecial(obj, id)
+						volunteersSpecial(obj, id)
 						console.log(response)
 					})
 			}
 		})
 }
 
-function volunteersSpecial(obj, userId) {
-	// get the most recently added Vol_id from Volunteers table
-	// client
-	// 	.from('Volunteers')
-	// 	.select('Vol_id')
-	// 	.order('Vol_id', { ascending: false })
-	// 	.limit(1)
-	// 	.then((response) => {
-	// 		if (obj.Role == "Medical_Vol") {
-	// 			// insert Vol_id into Medical_Vol table
-	// 			const Vol_id = response.data[0].Vol_id
-	// 			client
-	// 				.from('Medical_Vol')
-	// 				.insert([{ Vol_id: Vol_id}])
-	// 				.then((response) => {
-	// 					console.log(response)
-	// 				})
-	// 		}
-	// 	})
-
-	// check if a table named Volunteer exists in the database
-	client
-		.from("Volunteers")
-		.select('*')
+async function volunteersSpecial(obj, userId) {
+	await client
+		.from('Volunteers')
+		.select('Vol_id')
+		.order('Vol_id', { ascending: false })
+		.limit(1)
 		.then((response) => {
-			// if it exists print the response
-			console.log(response)
-		})
-		.catch((error) => {
-			console.log(error)
+			if (obj.Role == "Medical Volunteer") {
+				const Vol_id = response.data[0].Vol_id
+				client
+					.from('Medical_Vol')
+					.insert([{ Vol_id: Vol_id }])
+					.then((response) => {
+						console.log(response)
+					})
+			}
+
+			else if (obj.Role == "Finance Management") {
+				const Vol_id = response.data[0].Vol_id
+				client
+					.from('Finance_Vol')
+					.insert([{ Vol_id: Vol_id }])
+					.then((response) => {
+						console.log(response)
+					})
+			}
+
+			else if (obj.Role == "Educational Volunteer") {
+				const Vol_id = response.data[0].Vol_id
+				client
+					.from('Educational_Vol')
+					.insert([{ Vol_id: Vol_id }])
+					.then((response) => {
+						console.log(response)
+					})
+			}
+
+			else if (obj.Role == "Legal Aid Services") {
+				const Vol_id = response.data[0].Vol_id
+				client
+					.from('Legal_Vol')
+					.insert([{ Vol_id: Vol_id }])
+					.then((response) => {
+						console.log(response)
+					})
+			}
+
+			else if (obj.Role == "Media Management") {
+				const Vol_id = response.data[0].Vol_id
+				client
+					.from('MediaMan_Vol')
+					.insert([{ Vol_id: Vol_id }])
+					.then((response) => {
+						console.log(response)
+					})
+			}
 		})
 }
 
@@ -124,7 +150,6 @@ async function signup() {
 					setTimeout(() => {
 						window.location.href = "login.html"
 					}, 3000)
-					// end signup function execution
 					return false
 				}
 			})
