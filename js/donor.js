9bf841dce3bad9e4f996d3ff02fe13b05676f15f
ayihan <ayihanhaq@gmail.com>
2022-11-15 15:22:52 +0500

    added redeem points for donors

--- js/donor.js
+++ js/donor.js
@@ -1,3 +1,5 @@
+let userEmail
+
 const supa = init();
 
 function init() {
@@ -12,18 +14,19 @@ function editCredentials() {
 	let newPassword = prompt("Enter your new password", "password")
 	let Email = document.getElementById('email').innerHTML
 	Email = Email.substring(7)
+	userEmail = Email
 
-	updatePasswrod({Email, newPassword, currentPassword})
+	updatePasswrod({ Email, newPassword, currentPassword })
 }
 
 async function updatePasswrod(obj) {
-	client.auth.signInWithPassword({
+	supa.auth.signInWithPassword({
 		email: obj.Email,
 		password: obj.currentPassword
 	})
 		.then((response) => {
 			if (response.error == null) {
-				client.auth.updateUser({
+				supa.auth.updateUser({
 					email: obj.Email,
 					password: obj.newPassword
 				})
@@ -39,4 +42,147 @@ async function updatePasswrod(obj) {
 				alert("Incorrect password. Please try again")
 			}
 		})
+}
+
+async function redeemPoints() {
+	await supa.auth.getUser()
+		.then((response) => {
+			let logEmail = response.data.user.email
+			console.log(logEmail)
+			userEmail = logEmail
+		})
+	
+	console.log(userEmail)
+	let currentPoints = document.getElementById('currentpoints')
+	let userPoints = 0
+	// document.getElementById('cour1').addEventListener('click', () => {
+	console.log("clicked")
+	supa
+		.from('Users')
+		.select('id')
+		.eq('Email', userEmail)
+		.then((response) => {
+			console.log(response.data[0].id)
+			let id = response.data[0].id
+			supa
+				.from('Donors')
+				.select('points')
+				.eq('id', id)
+				.then((response) => {
+					currentPoints.innerHTML = "Current Points: " + response.data[0].points + " points"
+					userPoints = parseInt(response.data[0].points)
+				})
+		})
+
+	document.getElementById('cour1').addEventListener('click', () => {
+		if (userPoints < 1500) {
+			alert("You do not have enough points to redeem")
+		} else {
+			alert("You have redeemed 1500 points. Thanks for your contribution!")
+			userPoints -= 1500
+			console.log(userPoints)
+			supa
+				.from('Users')
+				.select('id')
+				.eq('Email', userEmail)
+				.then((response) => {
+					console.log(response)
+					console.log(response.data[0].id)
+					let id = response.data[0].id
+					supa
+						.from('Donors')
+						.update({ 'points': userPoints })
+						.eq('id', id)
+						.then((response) => {
+							console.log(response.error)
+							document.getElementById('currentpoints').innerHTML = "Current Points: " + userPoints + " points"
+						})
+				})
+		}
+	})
+
+	document.getElementById('cour2').addEventListener('click', () => {
+		if (userPoints < 500) {
+			alert("You do not have enough points to redeem")
+		} else {
+			alert("You have redeemed 500 points. Thanks for your contribution!")
+			userPoints -= 500
+			console.log(userPoints)
+			supa
+				.from('Users')
+				.select('id')
+				.eq('Email', userEmail)
+				.then((response) => {
+					console.log(response)
+					console.log(response.data[0].id)
+					let id = response.data[0].id
+					supa
+						.from('Donors')
+						.update({ 'points': userPoints })
+						.eq('id', id)
+						.then((response) => {
+							console.log(response.error)
+							document.getElementById('currentpoints').innerHTML = "Current Points: " + userPoints + " points"
+						})
+				})
+		}
+	})
+
+	document.getElementById('cour3').addEventListener('click', () => {
+		if (userPoints < 1000) {
+			alert("You do not have enough points to redeem")
+		} else {
+			alert("You have redeemed 1000 points. Thanks for your contribution!")
+			userPoints -= 1000
+			console.log(userPoints)
+			supa
+				.from('Users')
+				.select('id')
+				.eq('Email', userEmail)
+				.then((response) => {
+					console.log(response)
+					console.log(response.data[0].id)
+					let id = response.data[0].id
+					supa
+						.from('Donors')
+						.update({ 'points': userPoints })
+						.eq('id', id)
+						.then((response) => {
+							console.log(response.error)
+							document.getElementById('currentpoints').innerHTML = "Current Points: " + userPoints + " points"
+						})
+				})
+		}
+	})
+
+	document.getElementById('cour4').addEventListener('click', () => {
+		if (userPoints < 2500) {
+			alert("You do not have enough points to redeem")
+		} else {
+			alert("You have redeemed 2500 points. Thanks for your contribution!")
+			userPoints -= 2500
+			console.log(userPoints)
+			supa
+				.from('Users')
+				.select('id')
+				.eq('Email', userEmail)
+				.then((response) => {
+					console.log(response)
+					console.log(response.data[0].id)
+					let id = response.data[0].id
+					supa
+						.from('Donors')
+						.update({ 'points': userPoints })
+						.eq('id', id)
+						.then((response) => {
+							console.log(response.error)
+							document.getElementById('currentpoints').innerHTML = "Current Points: " + userPoints + " points"
+						})
+				})
+		}
+	})
+}
+
+if (window.location.href.includes("points.html")) {
+	redeemPoints()
 }
